e86b1a01ab5b2513b4cede3c439f9b77318d8f81
Alok Srivastava <70486168+alok-sriv@users.noreply.github.com>
2021-07-11 14:54:01 -0400

    01- Getting Started

--- 01- Getting Started/blog/routes.py
+++ 01- Getting Started/blog/routes.py
@@ -1,6 +1,11 @@
 from blog import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, All!"
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
