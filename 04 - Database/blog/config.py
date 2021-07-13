18301ddcd9ffc5660e1afec3528553ccf2476867
Alok Srivastava <70486168+alok-sriv@users.noreply.github.com>
2021-07-13 16:01:10 -0400

    Update config.py

--- 04 - Database/blog/config.py
+++ 04 - Database/blog/config.py
@@ -5,10 +5,12 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe()
     #  I prefer the "or" method, as that also works if the environment variable is set to an empty string.
 
-# Config MySQL
+   # Config MySQL
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
     MYSQL_DB = 'flask_blog'
-    MYSQL_CURSORCLASS = 'DictCursor'
 
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+MYSQL_USER+':'+MYSQL_PASSWORD+'@'+MYSQL_HOST+'/'+MYSQL_DB
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
