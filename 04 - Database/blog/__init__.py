da35786363d4a9acf4ecaa0a0ef230d717c1c7d3
Alok Srivastava <70486168+alok-sriv@users.noreply.github.com>
2021-07-13 22:01:38 -0400

    Update __init__.py

--- 04 - Database/blog/__init__.py
+++ 04 - Database/blog/__init__.py
@@ -2,12 +2,13 @@
 from flask import Flask
 from blog.config import Config
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 # print(f"In __init__.py: {__name__}") o/p : In __init__.py: blog
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from blog import routes, models
