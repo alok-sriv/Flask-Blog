2ea294e36380d3c083297d5a6b88c25082f59dfc
Alok Srivastava <70486168+alok-sriv@users.noreply.github.com>
2021-07-15 11:10:24 -0400

    Error Handling

---
+++ 07 - Error Handling/blog/__init__.py
@@ -0,0 +1,19 @@
+# FileName: Flask-Blog > blog > __init__.py
+from flask import Flask
+from blog.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+# print(f"In __init__.py: {__name__}") o/p : In __init__.py: blog
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+bcrypt = Bcrypt(app)
+
+
+from blog import routes, models, errors
---
+++ 07 - Error Handling/blog/config.py
@@ -0,0 +1,18 @@
+# FileName: Flask-Blog > blog > config.py
+import os, secrets
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe()
+    #  I prefer the "or" method, as that also works if the environment variable is set to an empty string.
+
+   # Config MySQL
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_DB = 'flask_blog'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+MYSQL_USER+':'+MYSQL_PASSWORD+'@'+MYSQL_HOST+'/'+MYSQL_DB
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
